6b7d229b1fcd9b8f6a4ca175436a7f9fd0d068ef
Alexander Glushchenko <a.glushchenko@cmachine.ru>
2022-06-05 11:20:48 +1000

    Added Data code and migrations

--- SixMinApi/SixMinApi/Program.cs
+++ SixMinApi/SixMinApi/Program.cs
@@ -1,10 +1,22 @@
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using SixMinApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+var sqlConBuilder = new SqlConnectionStringBuilder();
 
+sqlConBuilder.ConnectionString = builder.Configuration.GetConnectionString("SQLDbConnection");
+sqlConBuilder.UserID = builder.Configuration["UserId"];
+sqlConBuilder.Password = builder.Configuration["Password"];
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(sqlConBuilder.ConnectionString));
+builder.Services.AddScoped<ICommandRepo, CommandRepo>();
+
+var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
